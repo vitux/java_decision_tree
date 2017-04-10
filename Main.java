e4980e62eddfb44e6a258003a0a371a1f64c6acd
vitux <vitux1@mail.ru>
2017-04-11 02:48:26 +0300

    fix

--- Main.java
+++ Main.java
@@ -7,15 +7,21 @@ public class Main {
         Double ss = null;
         DecisionTree d = new DecisionTree();
         ArrayList<ArrayList<Double>> x = new ArrayList<ArrayList<Double>>();
-        x.add(new ArrayList<Double>(Arrays.asList(1., 2., 3.)));
-        x.add(new ArrayList<Double>(Arrays.asList(10., 20., 30.)));
-        ArrayList<Integer> y = new ArrayList<Integer>(Arrays.asList(1, 0));
+        x.add(new ArrayList<Double>(Arrays.asList(0., 0.)));
+        x.add(new ArrayList<Double>(Arrays.asList(0., 1.)));
+        x.add(new ArrayList<Double>(Arrays.asList(1., 0.)));
+        x.add(new ArrayList<Double>(Arrays.asList(1., 1.)));
+        ArrayList<Integer> y = new ArrayList<Integer>(Arrays.asList(1, 0, 1, 1));
 
         d.learn(x, y);
 
-        ArrayList<Double> example0 = new ArrayList<Double>(Arrays.asList(1., 2., 3.));
-        ArrayList<Double> example1 = new ArrayList<Double>(Arrays.asList(10., -2., 43.));
+        ArrayList<Double> example0 = new ArrayList<Double>(Arrays.asList(0., 0.));
+        ArrayList<Double> example1 = new ArrayList<Double>(Arrays.asList(0., 1.));
+        ArrayList<Double> example2 = new ArrayList<Double>(Arrays.asList(1., 0.));
+        ArrayList<Double> example3 = new ArrayList<Double>(Arrays.asList(1., 1.));
         System.out.println(d.predict(example0));
         System.out.println(d.predict(example1));
+        System.out.println(d.predict(example2));
+        System.out.println(d.predict(example3));
     }
 }
